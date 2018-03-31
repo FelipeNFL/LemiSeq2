40bf3d36e7c06b0ea29b8fff1359a02eb8349bcf
FelipeNFL <felipe.nunes@fdex.com.br>
2018-03-30 23:05:55 -0300

    Improvements infra and write README

--- lemiseq_auth/commom/defines.py
+++ lemiseq_auth/commom/defines.py
@@ -1,7 +1,21 @@
+import os
+import sys
+
+
+def get_environ(tag):
+    var = os.environ.get(tag)
+    if not var:
+        print(tag + " not defined on ENVIRON")
+        print("finishing...")
+        sys.exit()
+
+        return var
+
+
 _SECRET_KEY_ = 'essa key precisa ser trocada'
 
 _SERVER_URI_ = 'ldap://ldap-proxy.epm.br'
 _SEARCH_BASE_ = 'o=unifesp,c=br'
 
-_AUTH_HOST_ = 'lemiseq_auth'
-_AUTH_PORT_ = 5000
+_AUTH_HOST_ = get_environ('HOST')
+_AUTH_PORT_ = get_environ('PORT')
