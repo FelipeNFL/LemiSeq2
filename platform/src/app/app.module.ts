02b2f5b4a40577d0ecaf40b6bff4757e38995b59
FelipeNFL <felipe.nunes@fdex.com.br>
2018-05-12 13:23:47 -0300

    Remove incomplete root

--- platform/src/app/app.module.ts
+++ platform/src/app/app.module.ts
@@ -4,25 +4,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app-component/app.component';
 import { HeaderComponent } from './header/header.component' 
-import { appRoutes } from './routes';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { RootComponent } from './root/root.component';
-// import { AuthService } from './service/auth.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
-    RootComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
-  ],
-  providers: [
-    // AuthService
   ],
   bootstrap: [AppComponent]
 })
