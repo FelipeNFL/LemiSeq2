5fc9d86f9e410e201d4f08318630a5303fdf628e
FelipeNFL <felipe.nunes@fdex.com.br>
2018-06-23 16:36:15 -0300

    Add secret key to API's environment

--- bioprocess/core/defines.py
+++ bioprocess/core/defines.py
@@ -24,6 +24,8 @@ MONGO_DB = get_environment_variable('MONGO_DATABASE')
 BIOPROCESS_HOST = get_environment_variable('BIOPROCESS_HOST')
 BIOPROCESS_PORT = get_environment_variable('BIOPROCESS_PORT')
 
+SECRET_KEY = get_environment_variable('SECRET_KEY')
+
 DATA_PATH = 'data'
 
 DATA_CHROMPACK = DATA_PATH + '/chrompack'
