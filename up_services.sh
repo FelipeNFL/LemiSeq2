f408831c8a41240110087d7d445ff6f35212c62c
FelipeNFL <felipe.nunes@fdex.com.br>
2018-06-23 16:37:21 -0300

    Fix up_services script

--- up_services.sh
+++ up_services.sh
@@ -1,8 +1,6 @@
 if [ "$1" = "build" ]
 then
-    docker-compose -f bioprocess/docker-compose.yml up --build &&
-    docker-compose -f auth/docker-compose.yml up --build
+    docker-compose -f bioprocess/docker-compose.yml up --build & docker-compose -f auth/docker-compose.yml up --build
 else
-    docker-compose -f bioprocess/docker-compose.yml up &&
-    docker-compose -f auth/docker-compose.yml up
+    docker-compose -f bioprocess/docker-compose.yml up & docker-compose -f auth/docker-compose.yml up
 fi
